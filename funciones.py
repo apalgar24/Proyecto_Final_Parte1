4c1b157a5a9ef56272f300099f34525fef22edfa
apalgar24 <adrianpalomino68dh@hotmail.com>
2022-05-16 11:17:02 +0200

    ajustes

--- funciones.py
+++ funciones.py
@@ -11,3 +11,24 @@ def juegos_obtiene(key,id):
     for i in datos.get("response").get("games"):
         juegos.append(i.get("name"))
     return juegos
+
+def ultimo_juego(key,id,juego):
+    horas=[]
+    r=requests.get(f"https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={key}&steamid={id}&format=json")
+    datos = r.json()
+    for i in datos.get("response").get("games"):
+        if i.get("name") == juego:
+            horas.append(i.get("playtime_forever"))
+    return horas
+
+def info_user(key,id):
+    info=[]
+    r=requests.get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={key}&steamids={id}")
+    datos = r.json()
+    for i in datos.get("response").get("players"):
+        info.append(i.get("steamid"))
+        info.append(i.get("realname"))
+        info.append(i.get("loccountrycode"))
+    return info
+
+
--- principal.py
+++ principal.py
@@ -1,13 +1,39 @@
 import json
 from funciones import *
-
+id=os.environ["id"]
+key=os.environ["key"]
 print("Información del perfil Senutrio-Harivantan")
 #Menú de opciones
 menu='''
 1. Mostrar Biblioteca de juegos
-2. Ultimo juego jugado
+2. Horas de un videojuego
 3. Información del perfil
 4. Salir
 '''
 #Iniciar el programa
+print(menu)
+opcion = int(input("Seleccione una opción: "))
+while opcion != 4:
+    if opcion == 1:
+        print("Biblioteca de juegos")
+        for i in juegos_obtiene(key,id):
+            print("-->",i)
+        print()
+    elif opcion == 2:
+        juego=input("Ingrese el nombre del juego: ")
+        print(juego)
+        for i in ultimo_juego(key,id,juego):
+            print(i/60,"horas")
+    elif opcion == 3:
+        print()
+        print("---INFORMACIÓN USUARIO---")
+        print("- SteamID: ",info_user(key,id)[0])
+        print("- Nombre Real: ",info_user(key,id)[1])
+        print("- País: ",info_user(key,id)[2])
+    elif opcion == 4:
+        print("Gracias por utilizar el programa")
+    else:
+        print("Opción no valida")
+    print (menu)
+    opcion = int(input("Seleccione una opción: "))
 
